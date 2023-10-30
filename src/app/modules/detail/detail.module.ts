a45df633366668525dd37ab1b8b0298db77228e1
Alvaro <alvaro.barma@gmail.com>
2023-10-30 09:00:39 +0100

    changes added, service ignored

--- src/app/modules/detail/detail.module.ts
+++ src/app/modules/detail/detail.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from "@angular/core";
 import { DetailComponent } from './container/detail.component';
-import { DetailService } from './services/detail.service';
-import { CommonModule } from "@angular/common";
-import { HomeRoutingModule } from "../home/home-routing.module";
 import { DetailRoutingModule } from "./detail-routing.module";
+import { DetailService } from './services/detail.service';
 
 @NgModule({
     declarations: [DetailComponent],
     imports: [
-        CommonModule,
         DetailRoutingModule
     ],
     providers: [DetailService]
--- src/app/modules/detail/services/detail.service.ts
+++ src/app/modules/detail/services/detail.service.ts
@@ -1,40 +1,37 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, map } from "rxjs";
-import { CharacterResponse, Character } from "../../home/interfaces/character-response.interface";
-import { HomeCharacter } from "../../home/interfaces/home-character.interface";
-import { DetailCharacter } from "../interfaces/detail-character.interface";
 import { development } from "src/app/environments/environment.development";
+import { Character, CharacterResponse } from "../../home/interfaces/character-response.interface";
+import { DetailCharacter } from "../interfaces/detail-character.interface";
 
 
 @Injectable()
 export class DetailService {
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
+  
+  private baseUrl = `${development.apiUrl}/character/`;
 
-
-    
-    private baseUrl = `${development.apiUrl}/character/`;
-
-    getCharacterById(id: string): Observable<DetailCharacter[]> {
-      const url = `${this.baseUrl}${id}`;
-      return this.http.get<CharacterResponse>(url).pipe(
-        map<CharacterResponse, DetailCharacter[]>((characterResponse: CharacterResponse) =>
-          characterResponse.results.map((character: Character) =>
-          ({
-            created:  character.created,
-            episode:  character.episode,
-            gender:   character.gender,
-            image:    character.image,
-            location: character.location.name,
-            name:     character.name,
-            origin:   character.origin.name,
-            species:  character.species,
-            status:   character.status,
-            type:     character.type,
-            url:      character.url
-          }))
-        )
-      );
-    }
+  getCharacterById(id: string): Observable<DetailCharacter[]> {
+    const url = `${this.baseUrl}${id}`;
+    return this.http.get<CharacterResponse>(url).pipe(
+      map<CharacterResponse, DetailCharacter[]>((characterResponse: CharacterResponse) =>
+        characterResponse.results.map((character: Character) =>
+        ({
+          created: character.created,
+          episode: character.episode,
+          gender: character.gender,
+          image: character.image,
+          location: character.location.name,
+          name: character.name,
+          origin: character.origin.name,
+          species: character.species,
+          status: character.status,
+          type: character.type,
+          url: character.url
+        }))
+      )
+    );
   }
+}
